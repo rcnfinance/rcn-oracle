52e5d7219208f784b85f7296aab8d9fcb1c2495a
Agustin Aguilar <agusxrun@gmail.com>
2019-07-16 18:08:06 +0200

    Add some test cases

---
+++ test/TestMultiSourceOracle.js
@@ -0,0 +1,155 @@
+const Oracle = artifacts.require('./MultiSourceOracle.sol');
+const Factory = artifacts.require('./OracleFactory.sol');
+
+const BN = web3.utils.BN;
+const expect = require('chai')
+    .use(require('bn-chai')(BN))
+    .expect;
+
+function bn(number) {
+    return new BN(number);
+}
+
+function perm(xs) {
+    let ret = [];
+
+    for (let i = 0; i < xs.length; i = i + 1) {
+        let rest = perm(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+        if (!rest.length) {
+            ret.push([xs[i]])
+        } else {
+            for (let j = 0; j < rest.length; j = j + 1) {
+                ret.push([xs[i]].concat(rest[j]))
+            }
+        }
+    }
+
+    return ret;
+}
+
+contract('Multi Source Oracle', function (accounts) {
+    before(async () => {
+        this.owner = accounts[9];
+        this.factory = await Factory.new({ from: this.owner });
+    });
+
+    async function createOracle(symbol) {
+        const event = await this.factory.newOracle(symbol, { from: this.owner });
+        return await Oracle.at(event.logs.find(l => l.event == 'NewOracle').args._oracle);
+    }
+
+    it("Should return single rate with a single provider", async () => {
+        const oracle = await createOracle("TEST-1");
+        await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+        await this.factory.provide(oracle.address, 100000);
+        const sample = await oracle.readSample();
+        expect(sample[1]).to.eq.BN(bn(100000));
+    });
+
+    it("Should return average rate with a two providers", async () => {
+        const oracle = await createOracle("TEST-2");
+        await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+        await this.factory.provide(oracle.address, 100000, { from: accounts[0] });
+        await this.factory.provide(oracle.address, 200000, { from: accounts[1] });
+        const sample = await oracle.readSample();
+        expect(sample[1]).to.eq.BN(bn(150000));
+    });
+
+    it("Should return the median rate with a three providers", async () => {
+        const oracle = await createOracle("TEST-3");
+        await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+        await this.factory.provide(oracle.address, 100000, { from: accounts[0] });
+        await this.factory.provide(oracle.address, 200000, { from: accounts[1] });
+        await this.factory.provide(oracle.address, 300000, { from: accounts[2] });
+        const sample = await oracle.readSample();
+        expect(sample[1]).to.eq.BN(bn(200000));
+    });
+
+    it("Should return the median rate with a three providers, regardless the order", async () => {
+        const provided = perm([200000, 100000, 300000]);
+        for (const i in provided) {
+            const provide = provided[i];
+            const oracle = await createOracle(`TEST-4-${i}`);
+            await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+            await this.factory.provide(oracle.address, provide[0], { from: accounts[1] });
+            await this.factory.provide(oracle.address, provide[1], { from: accounts[0] });
+            await this.factory.provide(oracle.address, provide[2], { from: accounts[2] });
+            const sample = await oracle.readSample();
+            expect(sample[1]).to.eq.BN(bn(200000));
+        }
+    });
+
+    it("Should return the average of the two median rate with a four providers", async () => {
+        const oracle = await createOracle("TEST-5");
+        await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[3], { from: this.owner });
+        await this.factory.provide(oracle.address, 100000, { from: accounts[0] });
+        await this.factory.provide(oracle.address, 200000, { from: accounts[1] });
+        await this.factory.provide(oracle.address, 300000, { from: accounts[2] });
+        await this.factory.provide(oracle.address, 400000, { from: accounts[3] });
+        const sample = await oracle.readSample();
+        expect(sample[1]).to.eq.BN(bn(250000));
+    });
+
+    it("Should return the average of the two median rate with a four providers, regardless the order", async () => {
+        const provided = perm([200000, 100000, 300000, 400000]);
+        for (const i in provided) {
+            const provide = provided[i];
+            const oracle = await createOracle(`TEST-6-${i}`);
+            await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[3], { from: this.owner });
+            await this.factory.provide(oracle.address, provide[0], { from: accounts[0] });
+            await this.factory.provide(oracle.address, provide[1], { from: accounts[1] });
+            await this.factory.provide(oracle.address, provide[2], { from: accounts[2] });
+            await this.factory.provide(oracle.address, provide[3], { from: accounts[3] });
+            const sample = await oracle.readSample();
+            expect(sample[1]).to.eq.BN(bn(250000));
+        }
+    });
+
+    it("Should return the median rate with a five providers", async () => {
+        const oracle = await createOracle("TEST-7");
+        await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[3], { from: this.owner });
+        await this.factory.addSigner(oracle.address, accounts[4], { from: this.owner });
+        await this.factory.provide(oracle.address, 100000, { from: accounts[0] });
+        await this.factory.provide(oracle.address, 200000, { from: accounts[1] });
+        await this.factory.provide(oracle.address, 300000, { from: accounts[2] });
+        await this.factory.provide(oracle.address, 400000, { from: accounts[3] });
+        await this.factory.provide(oracle.address, 500000, { from: accounts[4] });
+        const sample = await oracle.readSample();
+        expect(sample[1]).to.eq.BN(bn(300000));
+    });
+
+    it("Should return the median rate with a five providers, regardless the order", async () => {
+        const provided = perm([200000, 100000, 300000, 400000, 500000]);
+        for (const i in provided) {
+            const provide = provided[i];
+            const oracle = await createOracle(`TEST-8-${i}`);
+            await this.factory.addSigner(oracle.address, accounts[0], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[1], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[2], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[3], { from: this.owner });
+            await this.factory.addSigner(oracle.address, accounts[4], { from: this.owner });
+            await this.factory.provide(oracle.address, provide[0], { from: accounts[0] });
+            await this.factory.provide(oracle.address, provide[1], { from: accounts[1] });
+            await this.factory.provide(oracle.address, provide[2], { from: accounts[2] });
+            await this.factory.provide(oracle.address, provide[3], { from: accounts[3] });
+            await this.factory.provide(oracle.address, provide[4], { from: accounts[4] });
+            const sample = await oracle.readSample();
+            expect(sample[1]).to.eq.BN(bn(300000));
+        }
+    });
+});
