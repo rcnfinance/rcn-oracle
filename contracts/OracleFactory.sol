d61dc7c07d39c711d6afb5b3a4d8c28831ebfaab
Agustin Aguilar <agusxrun@gmail.com>
2019-07-16 18:07:51 +0200

    Map currency symbols in factory

--- contracts/OracleFactory.sol
+++ contracts/OracleFactory.sol
@@ -5,31 +5,33 @@ import "./commons/Ownable.sol";
 
 
 contract OracleFactory is Ownable {
-    mapping(bytes32 => address) public currencyToOracle;
-    mapping(address => bytes32) public oracleToCurrency;
+    mapping(string => address) public symbolToOracle;
+    mapping(address => string) public oracleToSymbol;
 
-    event NewOracle(string _symbol, bytes32 _currency, address _oracle);
+    event NewOracle(string _symbol, address _oracle);
 
     function newOracle(string calldata _symbol) external onlyOwner {
-        MultiSourceRateOracle oracle = new MultiSourceRateOracle();
-        bytes32 currency = abi.decode(bytes(_symbol), (bytes32));
-        currencyToOracle[currency] = address(oracle);
-        oracleToCurrency[address(oracle)] = currency;
-        emit NewOracle(_symbol, currency, address(oracle));
+        // Create oracle contract
+        MultiSourceOracle oracle = new MultiSourceOracle();
+        // Save Oracle in registry
+        symbolToOracle[_symbol] = address(oracle);
+        oracleToSymbol[address(oracle)] = _symbol;
+        // Emit events
+        emit NewOracle(_symbol, address(oracle));
     }
 
     function addSigner(address _oracle, address _signer) external onlyOwner {
-        MultiSourceRateOracle(_oracle).addSigner(_signer);
+        MultiSourceOracle(_oracle).addSigner(_signer);
     }
 
     function provide(address _oracle, uint256 _rate) external {
-        MultiSourceRateOracle(_oracle).provide(msg.sender, _rate);
+        MultiSourceOracle(_oracle).provide(msg.sender, _rate);
     }
 
     function provideMultiple(bytes32[] calldata _data) external {
         for (uint256 i = 0; i < _data.length; i++) {
             (address oracle, uint256 rate) = _decode(_data[i]);
-            MultiSourceRateOracle(oracle).provide(msg.sender, rate);
+            MultiSourceOracle(oracle).provide(msg.sender, rate);
         }
     }
 
