346a3caf3688679cb7ec24fe2c7c02be16383abe
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-08-15 16:19:07 -0300

    WIP sorted-collection integration

--- contracts/MultiSourceOracle.sol
+++ contracts/MultiSourceOracle.sol
@@ -1,48 +1,38 @@
 pragma solidity ^0.5.10;
 
 import "./commons/Ownable.sol";
-import "./commons/AddressHeap.sol";
+import "sorted-collection/SortedList.sol";
+import "sorted-collection/SortedListDelegate.sol";
 import "./interfaces/RateOracle.sol";
 import "./interfaces/UpgradeProvider.sol";
 
 
-contract MultiSourceOracle is Ownable {
-    using AddressHeap for AddressHeap.Heap;
+contract MultiSourceOracle is Ownable, SortedListDelegate {
 
+    using SortedList for SortedList.List;
     uint256 public constant BASE = 10 ** 18;
+    uint256 public internalId = 0;
 
     event Upgraded(address _prev, address _new);
     event AddSigner(address _signer, string _name);
     event UpdateName(address _signer, string _oldName, string _newName);
     event RemoveSigner(address _signer, string _name);
 
+    mapping(uint256 => uint256) internal nodes;
     mapping(address => bool) public isSigner;
     mapping(address => string) public nameOfSigner;
     mapping(string => address) public signerWithName;
-    AddressHeap.Heap private topProposers;
-    AddressHeap.Heap private botProposers;
+    mapping(address => uint256) private signerWithNode;
 
+    
+    SortedList.List private list;
     RateOracle public upgrade;
 
-    constructor() public {
-        topProposers.initialize(true);
-        botProposers.initialize(false);
-    }
+    constructor() public {}
 
-    function getProvided(address _addr) external view returns (
-        bool _topHeap,
-        bool _botHeap,
-        uint256 _rate,
-        uint256 _indexHeap
-    ) {
-        _topHeap = topProposers.has(_addr);
-        _botHeap = botProposers.has(_addr);
-
-        if (_topHeap) {
-            (_indexHeap, _rate) = topProposers.getAddr(_addr);
-        } else if (_botHeap) {
-            (_indexHeap, _rate) = botProposers.getAddr(_addr);
-        }
+    function getProvided(address _addr) external view returns (uint256 _rate, uint256 _index) {
+        uint256 id = signerWithNode[_addr];
+        return (this.getValue(id), id);
     }
 
     function setUpgrade(RateOracle _upgrade) external onlyOwner {
@@ -72,40 +62,18 @@ contract MultiSourceOracle is Ownable {
     }
 
     function removeSigner(address _signer) external onlyOwner {
+        
         string memory name = nameOfSigner[_signer];
-        emit RemoveSigner(_signer, name);
+        if (!isSigner[_signer]) {
+            return;
+        }
+        
+        isSigner[_signer] = false;
         signerWithName[name] = address(0);
+        uint256 id = signerWithNode[_signer];
+        list.remove(id);
+        emit RemoveSigner(_signer, name);
 
-        if (isSigner[_signer]) {
-            isSigner[_signer] = false;
-        }
-
-        if (topProposers.has(_signer)) {
-            // Send to bottom and pop
-            topProposers.update(_signer, 0);
-            topProposers.popTop();
-        } else if (botProposers.has(_signer)) {
-            // Send to top and pop
-            botProposers.update(_signer, uint96(uint256(-1)));
-            botProposers.popTop();
-        }
-
-        uint256 topSize = topProposers.size();
-        uint256 botSize = botProposers.size();
-
-        if (topSize != botSize) {
-            if (topSize > botSize + 1) {
-                (address topAddr, uint256 topValue) = topProposers.top();
-                topProposers.popTop();
-                botProposers.insert(topAddr, topValue);
-            } else if (botSize > topSize + 1) {
-                (address botAddr, uint256 botValue) = botProposers.top();
-                botProposers.popTop();
-                topProposers.insert(botAddr, botValue);
-            }
-        }
-
-        _equilibrate();
     }
 
     function provide(address _signer, uint256 _rate) external onlyOwner {
@@ -113,15 +81,9 @@ contract MultiSourceOracle is Ownable {
         require(_rate != 0, "rate can't be zero");
         require(_rate < uint96(uint256(-1)), "rate too high");
 
-        if (topProposers.has(_signer)) {
-            topProposers.update(_signer, _rate);
-        } else if (botProposers.has(_signer)) {
-            botProposers.update(_signer, _rate);
-        } else {
-            _insert(_signer, _rate);
-        }
-
-        _equilibrate();
+        uint256 id = newNode(_rate);
+        signerWithNode[_signer] = id;
+        list.insert(id, address(this));
     }
 
     function readSample(bytes calldata) external view returns (uint256, uint256) {
@@ -137,48 +99,17 @@ contract MultiSourceOracle is Ownable {
 
         // Tokens is always base
         _tokens = BASE;
-
-        uint256 topSize = topProposers.size();
-        uint256 botSize = botProposers.size();
-
-        if (topSize > botSize) {
-            (, _equivalent) = topProposers.top();
-        } else if (botSize > topSize) {
-            (, _equivalent) = botProposers.top();
-        } else {
-            // Calculate equivalent
-            (, uint256 topValue) = topProposers.top();
-            (, uint256 botValue) = botProposers.top();
-            _equivalent = (topValue + botValue) / 2;
-        }
+        _equivalent = list.median(address(this));
     }
 
-    function _insert(address _signer, uint256 _rate) private {
-        uint256 topSize = topProposers.size();
-        uint256 botSize = botProposers.size();
-
-        if (botSize < topSize) {
-            botProposers.insert(_signer, _rate);
-        } else {
-            topProposers.insert(_signer, _rate);
-        }
+    function getValue(uint256 id) external view returns (uint256) {
+        return nodes[id];
     }
 
-    function _equilibrate() private {
-        (address topAddr, uint256 topValue) = topProposers.top();
-        (address botAddr, uint256 botValue) = botProposers.top();
-
-        if (topAddr == address(0) || botAddr == address(0)) {
-            return;
-        }
-
-        if (topValue < botValue) {
-            // Swap tops
-            topProposers.popTop();
-            botProposers.popTop();
-            // Insert reverted
-            topProposers.insert(botAddr, botValue);
-            botProposers.insert(topAddr, topValue);
-        }
+    function newNode(uint256 _value) private returns (uint256) {
+        internalId = internalId + 1;
+        nodes[internalId] = _value;
+        return internalId;
     }
+
 }
