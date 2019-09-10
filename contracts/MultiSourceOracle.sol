932029983bea2619ec31cd79d78925ed8c4b1922
rotcivegaf <victorfage@gmail.com>
2019-09-10 17:37:22 -0300

    move require to first

--- contracts/MultiSourceOracle.sol
+++ contracts/MultiSourceOracle.sol
@@ -174,8 +174,8 @@ contract MultiSourceOracle is RateOracle, Ownable, Pausable {
      * @param _signer Address of the signer
      */
     function removeSigner(address _signer) external onlyOwner {
-        string memory signerName = nameOfSigner[_signer];
         require(isSigner[_signer], "address is not a signer");
+        string memory signerName = nameOfSigner[_signer];
 
         isSigner[_signer] = false;
         signerWithName[signerName] = address(0);
